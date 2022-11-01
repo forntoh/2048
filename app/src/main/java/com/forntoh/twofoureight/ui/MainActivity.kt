2feb7997cbe4218217559fd669519c4e71db93c3
Forntoh Thomas <thomasforntoh@gmail.com>
2022-11-01 14:32:20 +0100

    time

--- app/src/main/java/com/forntoh/twofoureight/ui/MainActivity.kt
+++ app/src/main/java/com/forntoh/twofoureight/ui/MainActivity.kt
@@ -6,11 +6,18 @@ import androidx.activity.compose.setContent
 import androidx.core.view.WindowCompat
 import com.forntoh.twofoureight.model.Game
 import com.forntoh.twofoureight.store.PreferenceRepository
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.*
+import java.util.*
+import kotlin.time.Duration
+import kotlin.time.ExperimentalTime
 
 class MainActivity : ComponentActivity() {
 
     private val preferenceRepository: PreferenceRepository by lazy { PreferenceRepository(this) }
 
+    @ExperimentalTime
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -22,12 +29,36 @@ class MainActivity : ComponentActivity() {
             onMove = { preferenceRepository.moves++ }
         )
 
+        preferenceRepository.useSystemUiMode = true
+
         setContent {
             GameApp(
                 preferenceRepository = preferenceRepository,
                 game = game
             )
         }
+
+        tickerFlow(Duration.seconds(1))
+            .map { Calendar.getInstance() }
+            .distinctUntilChanged { old, new ->
+                old.get(Calendar.SECOND) == new.get(Calendar.SECOND)
+            }
+            .onEach {
+                setDateTime(it)
+            }
+            .launchIn(GlobalScope)
     }
 
+}
+
+@ExperimentalTime
+fun tickerFlow(
+    period: Duration,
+    initialDelay: Duration = Duration.ZERO
+) = flow {
+    delay(initialDelay)
+    while (true) {
+        emit(Unit)
+        delay(period)
+    }
 }
