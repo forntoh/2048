00f382919de1fe20af00db739f2f2fc173fd16c5
forntoh <thomasforntoh@gmail.com>
2022-11-01 16:04:28 +0100

    added time elapsed to game app

--- app/src/main/java/com/forntoh/twofoureight/ui/GameApp.kt
+++ app/src/main/java/com/forntoh/twofoureight/ui/GameApp.kt
@@ -1,8 +1,11 @@
 package com.forntoh.twofoureight.ui
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Scaffold
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Modifier
 import com.forntoh.twofoureight.model.Game
 import com.forntoh.twofoureight.store.PreferenceRepository
 import com.forntoh.twofoureight.ui.play.PlayScreen
@@ -16,20 +19,24 @@ fun GameApp(
 ) {
     ProvideWindowInsets {
         GameTheme(darkTheme = preferenceRepository.isDarkTheme) {
-            Scaffold {
+            Scaffold { padding ->
 
                 val score by preferenceRepository.scoreLive.observeAsState(0)
                 val highScore by preferenceRepository.highScoreLive.observeAsState(0)
                 val moves by preferenceRepository.movesLive.observeAsState(0)
+                val timeElapsed by preferenceRepository.timeElapsedLive.observeAsState(0)
 
                 PlayScreen(
                     score = score,
                     bestScore = highScore,
                     moves = moves,
+                    timeElapsed = timeElapsed,
                     game = game,
+                    modifier = Modifier.padding(padding),
                     onNewRequest = {
                         preferenceRepository.score = 0
                         preferenceRepository.moves = 0
+                        preferenceRepository.timeElapsed = 0
                         game.restart()
                     },
                     onUndoRequest = {
