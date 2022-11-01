5e5c0ff7e3adfd61b15a57efead28e72a496105b
forntoh <thomasforntoh@gmail.com>
2022-11-01 14:30:34 +0100

    fixed build

--- app/src/main/java/com/forntoh/twofoureight/GameApp.kt
+++ app/src/main/java/com/forntoh/twofoureight/GameApp.kt
@@ -1,10 +1,12 @@
 package com.forntoh.twofoureight
 
+import android.annotation.SuppressLint
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import com.forntoh.twofoureight.ui.components.GameBoard
 import com.forntoh.twofoureight.ui.theme.GameTheme
 
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun GameApp() {
     GameTheme {
